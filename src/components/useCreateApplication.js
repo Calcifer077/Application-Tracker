1dd7a347655600b055f7949bd8b0efe0a4b54020
Calcifer077 <maheshnashier14@gmail.com>
2025-06-22 15:26:49 +0530

    Fixed a bug while invalidating queries

--- src/components/useCreateApplication.js
+++ src/components/useCreateApplication.js
@@ -1,13 +1,22 @@
-import { QueryClient, useMutation } from "@tanstack/react-query";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { createApplication as createApplicationApi } from "../services/apiApplications";
+import toast from "react-hot-toast";
 
 export function useCreateApplication() {
+  // Get queryclient from the context
+  const queryClient = useQueryClient();
   const { mutate: createApplication, isLoading } = useMutation({
     mutationFn: createApplicationApi,
-    onSuccess: () =>
-      QueryClient.invalidateQueries({
+    onSuccess: () => {
+      toast.success("New Application successfully added!");
+
+      queryClient.invalidateQueries({
         queryKey: ["applications"],
-      }),
+      });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
   });
 
   return { isLoading, createApplication };
