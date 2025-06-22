6e0bc2ac35d5bd150b3a7d82d874b739538ef2d4
Calcifer077 <maheshnashier14@gmail.com>
2025-06-22 15:27:14 +0530

    Improved some styling

--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const StyledSelect = styled.select`
+const StyledSelectForm = styled.select`
   margin-top: 8px;
   padding: 10px 14px;
   background-color: white;
@@ -14,16 +14,58 @@ const StyledSelect = styled.select`
   }
 `;
 
-function Select({ options, value, onChange }) {
-  return (
-    <StyledSelect value={value} onChange={onChange}>
-      {options.map((option) => (
-        <option value={option.value} key={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </StyledSelect>
-  );
+const StyledSelectDetails = styled.select`
+  padding: 10px 14px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: var(--color-cell-background-color);
+  max-width: 160px;
+  font-size: 16px;
+  color: #1f2937;
+  outline: none;
+  transition: border-color 0.2s, box-shadow 0.2s;
+
+  &:hover {
+    border-color: #999;
+  }
+
+  &:focus {
+    border-color: var(--color-green);
+    box-shadow: 0 0 0 2px rgba(34, 197, 94, 0.3); /* green glow */
+  }
+
+  option {
+    background-color: white;
+    color: #1f2937;
+  }
+`;
+
+function Select({ options, value, onChange, type, disabled }) {
+  if (type === "form") {
+    return (
+      <StyledSelectForm value={value} onChange={onChange}>
+        {options.map((option) => (
+          <option value={option.value} key={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </StyledSelectForm>
+    );
+  } else {
+    return (
+      <StyledSelectDetails
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      >
+        {options.map((option) => (
+          <option value={option.value} key={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </StyledSelectDetails>
+    );
+  }
 }
 
 export default Select;
